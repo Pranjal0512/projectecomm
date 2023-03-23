95a1d0f422974eecf83bb83620bfb8ed808cc5e4
Pranjal0512 <pranjal.barnwal051234@gmail.com>
2023-03-23 11:33:58 +0545

    second commit

--- home/admin.py
+++ home/admin.py
@@ -4,5 +4,8 @@ admin.site.register(Service)
 admin.site.register(Contact)
 admin.site.register(Information)
 admin.site.register(Ad)
+admin.site.register(Product)
+admin.site.register(Type)
+admin.site.register(Category)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Service(models.Model):
     title= models.CharField(max_length=300)
     logo = models.CharField(max_length=300)
@@ -33,5 +34,36 @@ class Ad(models.Model):
     image = models.ImageField(upload_to='media')
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=500,unique=True)
+    def __str__(self):
+        return self.name
+
+class Type(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    slug = models.CharField(max_length=500,unique=True)
+
+    def __str__(self):
+        return self.name
+
+STATUS = (('Active','Active'),('Inactive','Inactive'))
+LABELS = (('new','new'),('featured','featured'),('all','all'),('offer','offer'),('','default'))
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField()
+    image = models.ImageField(upload_to='media')
+    description = models.TextField()
+    specification = models.TextField()
+    slug = models.CharField(max_length=500,unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    labels = models.CharField(choices=LABELS,max_length=50)
+    status = models.CharField(choices=STATUS, max_length=50)
     def __str__(self):
         return self.name
